ac82edaaf336ffc51817dbc7f6adb5e7977ebc3d
Rudinei Silva <rudislpp@gmail.com>
2022-10-24 23:02:13 -0300

    UI

---
+++ components/BuyTable/index.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import { Container, Header, OrderList, OrderListPrice,  OrderListAmount, OrderListTotal } from './styles';
+
+const BuyTable: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <div>Price(USDT)</div>
+        <div>Amount(BTC)</div>
+        <div>Total</div>
+      </Header>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+      <OrderList>
+        <OrderListPrice>19274.79</OrderListPrice>
+        <OrderListAmount>0.00498</OrderListAmount>
+        <OrderListTotal>176.92844</OrderListTotal>
+      </OrderList>
+  </Container>
+  )
+}
+
+export default BuyTable;
---
+++ components/BuyTable/styles.ts
@@ -0,0 +1,44 @@
+import styled from "@emotion/styled";
+
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 12px;
+  padding: 16px;
+  border: 1px solid rgb(31, 33, 36);
+  max-width: 320px;
+
+`;
+
+export const Header = styled.div`
+  display: grid;
+  grid-template-columns: 96px 96px 96px;
+  color: rgb(132, 142, 156);
+
+  div:last-child {
+    display: flex;
+    flex: 1 1 0%;
+    justify-content: flex-end;
+  }
+`;
+
+export const OrderList = styled.div`
+  display: grid;
+  grid-template-columns: 96px 96px 96px;
+  line-height: 22px;
+`;
+
+export const OrderListPrice = styled.div`
+
+`;
+export const OrderListAmount = styled.div`
+    display: flex;
+    flex: 1 1 0%;
+    justify-content: center;
+`;
+export const OrderListTotal = styled.div`
+    display: flex;
+    flex: 1 1 0%;
+    justify-content: flex-end;
+`;
